4b283491c6afaa5b19771910cb8951c62b07ee60
fakhrulhasan99 <fakhrulhasan.faf1@gmail.com>
2026-02-17 08:51:31 +0600

    products page added

--- js/main.js
+++ js/main.js
@@ -1,59 +1,55 @@
 const productContainer = document.getElementById("productContainer");
-const viewAllBtn = document.getElementById("viewAllBtn");
 
 let allProducts = [];
 
-// Fetch products
-const fetchProducts = async () => {
-  try {
-    const res = await fetch("https://fakestoreapi.com/products");
-    if (!res.ok) throw new Error("Failed to fetch products");
-
-    const data = await res.json();
-    allProducts = data;
-
-    showTrendingProducts(); // Show first 3 initially
-  } catch (error) {
-    productContainer.innerHTML =
-      `<p class="text-red-500">${error.message}</p>`;
-  }
+const fetchProducts = () => {
+    fetch("https://fakestoreapi.com/products")
+        .then(res => res.json())
+        .then(data => {
+            allProducts = data;
+            showTrendingProducts();
+        });
 };
 
-// Show top 3 products
 const showTrendingProducts = () => {
-  const topThree = allProducts.slice(0, 3);
-  renderProducts(topThree);
+    const topThree = allProducts.slice(0, 3);
+    renderProducts(topThree);
 };
 
-// Show all products
-const showAllProducts = () => {
-  renderProducts(allProducts);
-};
-
-// Render function
 const renderProducts = (products) => {
-  productContainer.innerHTML = "";
+    productContainer.innerHTML = "";
 
-  products.forEach(product => {
-    const { image, title, price } = product;
+    products.forEach(product => {
+        const { image, title, price, category, rating } = product;
 
-    const card = document.createElement("div");
-    card.className =
-      "border rounded-xl p-4 shadow hover:shadow-lg transition";
+        const card = document.createElement("div");
+        card.className =
+            "border border-slate-300 rounded-2xl p-4 shadow flex flex-col justify-between";
 
-    card.innerHTML = `
-        <img src="${image}" class="h-40 mx-auto object-contain mb-4" />
-        <h3 class="font-semibold text-lg mb-2 line-clamp-2">${title}</h3>
-        <p class="text-violet-600 font-bold mb-3">$${price}</p>
-        <button class="btn btn-sm btn-primary w-full">Add to Cart</button>
+        card.innerHTML = `
+      <img src="${image}" class="h-40 w-full mx-auto object-contain mb-4 bg-slate-300 py-4 rounded-2xl" />
+      <div class="flex justify-between mb-2">
+        <p class="text-violet-800 bg-violet-100 px-2 rounded-full">${category}</p>
+        <div class="flex items-center gap-2">
+            <i class="fa-solid fa-star text-yellow-400"></i>
+            <p class="text-slate-600">${rating.rate}</p>
+            <p class="text-slate-600">(${rating.count})</p>
+        </div>
+      </div>
+      <h3 class="font-semibold mb-2">${title}</h3>
+      <p class="text-violet-600 font-bold mb-3">$${price}</p>
+      <div class="flex justify-between">
+        <button class="btn btn-sm w-5/12">
+            <i class="fa-regular fa-eye"></i> Details
+        </button>
+        <button class="btn btn-sm btn-primary w-5/12">
+            <i class="fa-solid fa-cart-arrow-down"></i> Add
+        </button>
+      </div>
     `;
 
-    productContainer.appendChild(card);
-  });
+        productContainer.appendChild(card);
+    });
 };
 
-// Button event
-viewAllBtn.addEventListener("click", showAllProducts);
-
-// Initial call
 fetchProducts();
--- js/products.js
+++ js/products.js
@@ -1,36 +1,44 @@
 const container = document.getElementById("allProductsContainer");
 
-const fetchProducts = async () => {
-    try {
-        const res = await fetch("https://fakestoreapi.com/products");
+const fetchProducts = () => {
+    fetch("https://fakestoreapi.com/products")
+        .then(res => res.json())
+        .then(data => {
 
-        if (!res.ok) throw new Error("Failed to fetch products");
-
-        const products = await res.json();
-        renderProducts(products);
-
-    } catch (error) {
-        container.innerHTML =
-            `<p class="text-red-500">${error.message}</p>`;
-    }
+            renderProducts(data);
+        });
 };
 
 const renderProducts = (products) => {
     container.innerHTML = "";
 
     products.forEach(product => {
-        const { image, title, price } = product;
+        const { image, title, price, category, rating } = product;
 
         const card = document.createElement("div");
-        card.className = "border rounded-xl p-4 shadow";
+        card.className =
+            "border border-slate-300 rounded-2xl p-4 shadow flex flex-col justify-between";
 
         card.innerHTML = `
-        <img src="${image}" class="h-40 mx-auto object-contain mb-4" />
-        <h3 class="font-semibold mb-2">${title}</h3>
-        <p class="text-violet-600 font-bold mb-3">$${price}</p>
-        <button class="btn btn-sm btn-primary w-full">
-            Add to Cart
+      <img src="${image}" class="h-40 w-full mx-auto object-contain mb-4 bg-slate-300 py-4 rounded-2xl" />
+      <div class="flex justify-between mb-2">
+        <p class="text-violet-800 bg-violet-100 px-2 rounded-full md:text-xs">${category}</p>
+        <div class="flex items-center gap-2">
+            <i class="fa-solid fa-star text-yellow-400"></i>
+            <p class="text-slate-600">${rating.rate}</p>
+            <p class="text-slate-600">(${rating.count})</p>
+        </div>
+      </div>
+      <h3 class="font-semibold mb-2">${title}</h3>
+      <p class="text-violet-600 font-bold mb-3">$${price}</p>
+      <div class="flex justify-between">
+        <button class="btn btn-sm w-5/12">
+            <i class="fa-regular fa-eye"></i> Details
         </button>
+        <button class="btn btn-sm btn-primary w-5/12">
+            <i class="fa-solid fa-cart-arrow-down"></i> Add
+        </button>
+      </div>
     `;
 
         container.appendChild(card);
